04483fd32418132b0b3269e3f4f7c8f865b36af8
Seweryn <sewcio0212@gmail.com>
2024-12-06 17:00:49 +0100

    git reset --hard be7f828

--- backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba44f3a44d25e547ac690231ae5f4ad36f09710d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be7f828679820a2183a39070eecd012a8cd84c23")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
