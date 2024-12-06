961c14f948657d8f8faa630e0dc0ef14905578f0
Seweryn <sewcio0212@gmail.com>
2024-12-06 18:43:07 +0100

    downloaded litedb and rebuilded backend

--- backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1ac74fae44fd12a66b36659a5a1e3cf1286a9d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec98674a46682f4a4aeac05448535553a8a9fe04")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
